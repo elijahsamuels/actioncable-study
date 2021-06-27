7fa3213b3b4f56dcd9433491ba88a89410488c9d
Elijah Samuels <elijahsamuels@gmail.com>
2021-06-27 15:23:21 -0600

    making rooms dynamic

--- README.md
+++ README.md
@@ -32,3 +32,13 @@ $ rails s
 * Other notes
 - ActionCable in it's simplest explaination is WebSockets for Rails.
 
+
+
+
+<!-- go into a rails console -->
+- $ rails c 
+<!-- ActionCable.server.broadcast takes two parameters: "channel_name", "message" -->
+<!-- this way sends basic text -->
+$ ActionCable.server.broadcast "room_channel", "testing hello" 
+<!-- but you can also send POJO -->
+$ ActionCable.server.broadcast "room_channel", message: "hello"
--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,16 +1,28 @@
-import consumer from "./consumer"
+import consumer from "./consumer";
 
-consumer.subscriptions.create("RoomChannel", {
-  connected() {
-    console.log("connected to Room Channel")
-    // Called when the subscription is ready for use on the server
-  },
+document.addEventListener("turbolinks:load", () => {
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+  const element = document.getElementById('room-id');
+  const room_id = element.getAttribute('data-room-id');
 
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
-  }
+  console.log("room_id:", room_id)
+
+    consumer.subscriptions.create(
+        { channel: "RoomChannel", room_id: room_id },
+        {
+            connected() {
+                console.log("connected to room channel " + room_id);
+                // Called when the subscription is ready for use on the server
+            },
+
+            disconnected() {
+                // Called when the subscription has been terminated by the server
+            },
+
+            received(data) {
+                // Called when there's incoming data on the websocket for this channel
+                console.log(data);
+            },
+        }
+    );
 });
